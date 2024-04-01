f28cac0c78c62810bacad5fa9bfe5ee3e082440c
FILATECH <118884831+RamadanToure@users.noreply.github.com>
2024-04-01 16:32:04 +0000

    update

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\CV;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
     public function showSearchForm()
     {
-        return view('search');
+        $cvs = CV::all();
+        return view('cv.index', compact('cvs'));
     }
 
     public function search(Request $request)
@@ -20,7 +22,26 @@ class SearchController extends Controller
         $location = $request->input('location');
 
         // Implémentez la logique de recherche en fonction des critères
+        $query = CV::query();
+
+        if ($experience) {
+            $query->where('experience', $experience);
+        }
+
+        if ($skills && is_array($skills)) { // Vérifiez si $skills est un tableau
+            $query->whereIn('skill', $skills);
+        }
+
+        if ($location) {
+            $query->where('location', $location);
+        }
+
+        // Exécutez la requête et récupérez les résultats
+        $cvs = $query->get();
 
         // Retournez les résultats de la recherche à la vue
+        return view('cv.index', compact('cvs'));
     }
+
+
 }
--- resources/views/cv/index.blade.php
+++ resources/views/cv/index.blade.php
@@ -1,57 +1,103 @@
 <x-app-layout>
     <x-slot name="header">
         <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
+            <div class="row justify-content-center">
+                <div class="col-md-12">
                     <div class="card">
-<h1>yttshdz</h1>
-                        <div class="card-body">
-                            <table class="table table-bordered">
-                                <thead class="table-header-blue">
-                                    <tr>
-                                        <th>ID</th>
-                                        <th>Nom Complet</th>
-                                        <th>Numéro de Téléphone</th>
-                                        <th>Poste Actuel</th>
-                                        <th>Années d'Expérience</th>
-                                        <th>Dernier Diplôme</th>
-                                        <th>Domaine d'Études</th>
-                                        <th>Actions</th> <!-- Nouvelle colonne pour les actions -->
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach($cvs as $cv)
-                                    <tr>
-                                        <td>{{ $cv->id }}</td>
-                                        <td>{{ $cv->nom_complet }}</td>
-                                        <td>{{ $cv->numero_telephone }}</td>
-                                        <td>{{ $cv->poste_actuel }}</td>
-                                        <td>{{ $cv->annees_experience }}</td>
-                                        <td>{{ $cv->dernier_diplome }}</td>
-                                        <td>{{ $cv->domaine_etudes }}</td>
-                                        <td>
-                                            <a href="{{ route('cv.pdf', $cv->id) }}" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cvModal{{ $cv->id }}">
-                                                <i class="fas fa-eye"></i> Voir
-                                            </a>
-                                            <a href="{{ route('cv.edit', $cv->id) }}" class="btn btn-success">
-                                                <i class="fas fa-edit"></i>
-                                            </a>
-                                            @if(Auth::check() && Auth::user()->role === 'admin')
-                                            <button type="button" class="btn btn-danger delete-cv" data-cv-id="{{ $cv->id }}" style="background-color: red; color: white;">
-                                                <i class="fas fa-trash"></i> Supprimer
-                                            </button>
-                                            @endif
-                                        </td>
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
+                        <div class="card-header">
+                            <h2><b>Recherche Avancée de CV</b></h2>
+                        </div>
+                        <div class="card-body border">
+                            <form method="POST" action="{{ route('search') }}" class="row g-3">
+                                @csrf
+                                <div class="form-group row">
+                                    <div class="col-md-3">
+                                        <label for="experience" class="form-label"><b>Expérience :</b></label>
+                                        <input type="text" name="experience" id="experience" class="form-control" placeholder="Expérience">
+                                    </div>
+                                    <div class="col-md-3">
+                                        <label for="skills" class="form-label"><b>Compétences :</b></label>
+                                        <select name="skills" id="skills" class="form-control">
+                                            <option value="">Sélectionnez une compétence</option>
+                                            @isset($cvs)
+                                                @foreach($cvs as $cv)
+                                                    <option value="{{ $cv->dernier_diplome }}">{{ $cv->dernier_diplome }}</option>
+                                                @endforeach
+                                            @endisset
+                                        </select>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <label for="skills" class="form-label"><b>Domaine d'Étude :</b></label>
+                                        <select name="skills" id="skills" class="form-control">
+                                            <option value="">Sélectionnez un domaine d'étude</option>
+                                            @isset($cvs)
+                                                @foreach($cvs as $cv)
+                                                    <option value="{{ $cv->domaine_etudes }}">{{ $cv->domaine_etudes}}</option>
+                                                @endforeach
+                                            @endisset
+                                        </select>
+                                    </div>
+
+                                </div>
+                                <div class="form-group row">
+
+                                    <div class="col-md-6 d-flex align-items-end">
+                                        <button type="submit" class="btn btn-primary" style="background-color: rgb(57, 103, 228); color: white;">Rechercher</button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
     </x-slot>
+
+    <!-- Afficher le tableau des CV -->
+    <div class="card-body">
+        <table class="table table-bordered">
+            <thead class="table-header-blue">
+                <tr>
+                    <th>ID</th>
+                    <th>Nom Complet</th>
+                    <th>Numéro de Téléphone</th>
+                    <th>Poste Actuel</th>
+                    <th>Années d'Expérience</th>
+                    <th>Dernier Diplôme</th>
+                    <th>Domaine d'Études</th>
+                    <th>Actions</th> <!-- Nouvelle colonne pour les actions -->
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($cvs as $cv)
+                <tr>
+                    <td>{{ $cv->id }}</td>
+                    <td>{{ $cv->nom_complet }}</td>
+                    <td>{{ $cv->numero_telephone }}</td>
+                    <td>{{ $cv->poste_actuel }}</td>
+                    <td>{{ $cv->annees_experience }}</td>
+                    <td>{{ $cv->dernier_diplome }}</td>
+                    <td>{{ $cv->domaine_etudes }}</td>
+                    <td>
+                        <a href="{{ route('cv.pdf', $cv->id) }}" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cvModal{{ $cv->id }}">
+                            <i class="fas fa-eye"></i> Voir
+                        </a>
+                        <a href="{{ route('cv.edit', $cv->id) }}" class="btn btn-success">
+                            <i class="fas fa-edit"></i>
+                        </a>
+                        @if(Auth::check() && Auth::user()->role === 'admin')
+                        <button type="button" class="btn btn-danger delete-cv" data-cv-id="{{ $cv->id }}" style="background-color: red; color: white;">
+                            <i class="fas fa-trash"></i> Supprimer
+                        </button>
+                        @endif
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 
     <!-- Modal de confirmation de suppression -->
     @foreach($cvs as $cv)
@@ -68,9 +114,7 @@
                     Êtes-vous sûr de vouloir supprimer ce CV ?
                 </div>
                 <div class="modal-footer">
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
-                    </div>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
                     <!-- Bouton pour déclencher la suppression -->
                     <form action="{{ route('cv.destroy', $cv->id) }}" method="POST">
                         @csrf
